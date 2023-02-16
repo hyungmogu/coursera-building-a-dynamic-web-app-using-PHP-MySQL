b4dd36f4f9ad68169d9296c3fa2ce5bf1b189a5f
Hyungmo Gu <guhyungm7@gmail.com>
2023-02-15 18:43:07 -0700

    feat: Add notes for lesson 6

---
+++ 6_deleting_records_and_security/6_deleting_records_and_security.md
@@ -0,0 +1,8 @@
+# Deleting Records and Security
+
+1.
+
+## Aside
+
+1. Inserted database entry can be checked at URL `localhost/phpmyadmin`
+
---
+++ 6_deleting_records_and_security/code/index.php
@@ -0,0 +1,46 @@
+<?php
+    require_once("includes/db.php");
+
+    $sql = "SELECT * FROM notes";
+
+    $notes = mysqli_query($conn, $sql);
+?>
+<html>
+    <head>
+        <title>Notes App</title>
+        <link rel="stylesheet" href="styles/style.css">
+    </head>
+    <body>
+        <header> 
+            Notes App
+        </header>
+        <div>
+            <div>
+                <a class="nav-link" href="new.php">Add a new note</a>
+            </div>
+            <?php while($note = mysqli_fetch_assoc($notes)) {?>
+                <div class="note">
+                    <div class="titleContainer">
+                        <span class="nt-title"><?php $note["title"]; ?></span>
+                        <div class="nt-links">
+                            <a class="nt-link" href=<?php echo 'edit.php?id=' . $note['id'];?>>edit note</a>
+                            <a class="nt-link" href=<?php echo 'delete.php?id=' . $note['id'];?>>[X] delete note</a>
+                        </div>                 
+                    </div>
+                
+                    <div class="nt-content">
+                        <?php if ($note["important"]) {?>
+                            <span class="imp">IMPORTANT</span>
+                        <?php }?>
+                        <p>
+                          <?php echo $note["content"];?>  
+                        </p>
+                    </div>
+                </div>
+            <?php }?>
+        </div> 
+    </body>
+</html>
+<?php
+    require_once("includes/footer.php")
+?>
